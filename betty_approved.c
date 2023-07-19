86c760c83bf32b4917aac5d3a5a32487e2ff5150
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-07-19 19:43:57 +0100

    Did first assignment

---
+++ betty_approved.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @ac: number of arguments passed
+ * @av: pointer to arguments passed
+ *
+ * return: 0
+ */
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		printf("Usage: %s string ...\n", av[0]);
+		return (1);
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < ac; i++)
+		{
+			printf("argument %d: %s\n", i + 1, av[i]);
+		}
+	}
+	return (0);
+}
